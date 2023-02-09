4ea5c8cee3b8b13c14a02dc173dadd5c582d6bbe
Sri <srikanthsmedicherla@gmil.com>
2023-02-08 21:12:35 -0800

    edit: Clean up login page

--- job-tracker-frontend/src/components/dashboard/Login.tsx
+++ job-tracker-frontend/src/components/dashboard/Login.tsx
@@ -7,6 +7,8 @@ import {
   TextField,
   SxProps,
   Box,
+  Paper,
+  Typography,
 } from "@mui/material";
 const baseURL = "http://localhost:3003/authorize";
 function Login() {
@@ -14,32 +16,24 @@ function Login() {
 
   const authorizeUser = () => {
     Axios.get(baseURL).then((response) => {
+      // Move client to URL of redirect site>
       window.location.replace(response.data.url);
       const url = new URL(response.data.url);
       const codeId = url.searchParams.get("code");
-      console.log("response.request.responseURL: ", response.request);
-      console.log("The code is: ", response.data.url.href);
-      console.log("The code is: ", url);
-      console.log("The code is: ", codeId);
-
-      //   // setPosts(response.data);
-      //   console.log("localhost res is: ", response, response.data.url);
-      //   console.log(
-      //     "response.request.res.responseUrl res is: ",
-      //     response.request.res.responseUrl
-      //   );
-      //   setUrl_oauth(response.data.url);
     });
   };
-  const tokenUser = () => {
-    console.log("window.location.href: ", window.location.href);
-    console.log("next call..");
-  };
+  // Backup: Redirect is automatic so delete this later:
+  // const tokenUser = () => {
+  //   console.log("window.location.href: ", window.location.href);
+  //   console.log("next call..");
+  // };
   return (
     <>
-      <Button onClick={authorizeUser}>Sign in with Google</Button>
-      <Button onClick={tokenUser}>tokenUser</Button>
-      <a href={url_oauth}>Authorize to get Code</a>
+      <Box sx={{ maxWidth: "md" }}>
+        <Typography>Job Tracker Website</Typography>
+        <Button onClick={authorizeUser}>Sign in with Google</Button>
+      </Box>
+      {/* <a href={url_oauth}>Authorize to get Code</a> */}
       {/* {posts?.forEach((post) => (
         <div>
           <h1>{post.title}</h1>
