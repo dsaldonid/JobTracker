b4cf96c8738761479bc1d1cfdf96efbb48b75ac9
Sri <srikanthsmedicherla@gmil.com>
2023-02-19 17:56:18 -0800

    fix: move contacts table below session to trigger middleware first for contacts table

--- job-tracker-backend/server.js
+++ job-tracker-backend/server.js
@@ -4,38 +4,35 @@ const morgan = require("morgan");
 const authRoutes = require("./routes/altAuth");
 const { NotFoundError } = require("./utils/errors");
 const cookieParser = require("cookie-parser");
-const contactRoutes = require("./routes/contact")
+const contactRoutes = require("./routes/contact");
 
-const fs = require('fs')
-const psPool = require('./utils/psPool')
+const fs = require("fs");
+const psPool = require("./utils/psPool");
 
 // Create instance of express application
-const app = express()
+const app = express();
 
 // Enable cross-origin resource sharing for all origins
-app.use(cors())
+app.use(cors());
 
 // Parse request with JSON payload
-app.use(express.json())
+app.use(express.json());
 
 // Log request throughout app
-app.use(morgan("tiny"))
+app.use(morgan("tiny"));
 
 //Add routes for user authentication
-require('./routes/auth')(app);
+require("./routes/auth")(app);
 
 // Add routes for non-google user authentication
-app.use("/altAuth", authRoutes)
-
-// Add routes for contact functionality
-app.use("/contact", contactRoutes)
+app.use("/altAuth", authRoutes);
 
 // Creating one route
-app.get("/", async(req,res,next) =>{
-    res.status(200).json({'ping':'pong'})
-})
+app.get("/", async (req, res, next) => {
+  res.status(200).json({ ping: "pong" });
+});
 //Parse cookies for session management
-app.use(cookieParser())
+app.use(cookieParser());
 
 //Middleware function to verify sessionId and attach user information to request
 function sessionVerifier(req, res, next) {
@@ -68,14 +65,14 @@ function sessionVerifier(req, res, next) {
       if (err || ps_res.rows.length === 0) {
         if (err) console.log(err.stack);
 
-                res.status(401).json({ 'Error': 'Unauthorized due to invalid session' });
+        res.status(401).json({ Error: "Unauthorized due to invalid session" });
       } else {
         //req.userId can be used in downstream calls after middleware is used
         req.userId = ps_res.rows[0].userId;
-                next()
+        next();
       }
-        })
-    })
+    });
+  });
 }
 
 //Configure session middleware after authentication
@@ -84,27 +81,30 @@ app.use(sessionVerifier);
 //Calling the express.json() method for parsing JSON on future calls
 app.use(express.json());
 
+// Add routes for contact functionality
+app.use("/contact", contactRoutes);
+
 //Creates GET, POST, PUT and DELETE endpoints for /jobs
-require('./routes/jobs')(app);
+require("./routes/jobs")(app);
 
 // If no routes are touched then we will get a 404 error
 app.use((req, res, next) => {
-    return next(new NotFoundError())
-})
+  return next(new NotFoundError());
+});
 
 // Gives message on error as a responds if hit
 app.use((err, req, res, next) => {
-    const status = err.status || 500
-    const message = err. message
+  const status = err.status || 500;
+  const message = err.message;
 
   return res.status(status).json({
     error: { message, status },
-    })
-})
+  });
+});
 
 //process.env.PORT used for GCP
 const PORT = process.env.PORT || 3003;
 
 app.listen(PORT, () => {
-    console.log('Server listening in port ' + PORT)
-})
+  console.log("Server listening in port " + PORT);
+});
