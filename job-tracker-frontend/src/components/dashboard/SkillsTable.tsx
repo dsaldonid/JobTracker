a681b689ee44830939ef62ae4ef2f9d164a7bb8e
Scott Hung <scotthung1@gmail.com>
2023-02-25 20:08:50 -0800

    Implement add skill functionality and convert table into an observer component
    Still need to implement fetch functionality to reload the table upon successful request

--- job-tracker-frontend/src/components/dashboard/SkillsTable.tsx
+++ job-tracker-frontend/src/components/dashboard/SkillsTable.tsx
@@ -6,6 +6,14 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Title from './Title';
 import { Button, Grid, Input } from '@mui/material';
+import AppStore from "../app/AppStore";
+import { AppContext } from "../../index"
+import { observer } from "mobx-react-lite";
+import { randomId } from '@mui/x-data-grid-generator';
+import Axios from "axios";
+import { slotShouldForwardProp } from '@mui/material/styles/styled';
+
+const baseURL = "http://localhost:3003";
 
 // Generate Order Data
 function createData(
@@ -44,7 +52,40 @@ function preventDefault(event: React.MouseEvent) {
   event.preventDefault();
 }
 
-export default function SkillsTable() {
+interface Skill {
+  skillId?: string;
+  skillName: string;
+  comfortLevel: number;
+};
+
+const SkillsTable: React.FC = observer(() => {
+  const store: AppStore = React.useContext(AppContext);
+
+  const [skillName, setSkillName] = React.useState<string>('');
+  const [skillLevel, setSkillLevel] = React.useState<number>(0);
+  const [submitDisabled, setSubmitDisabled] = React.useState<boolean>(false);
+
+  const handleSubmit = () => {
+    setSubmitDisabled(true);
+
+    const newSkill: Skill = {
+      skillId: randomId(),
+      skillName: skillName,
+      comfortLevel: skillLevel,
+    }
+
+    Axios.post(`${baseURL}/skills`, newSkill, {
+      headers: {
+        Authorization: `Bearer ${store.session}`,
+      },
+    }).then((response) => {
+      setSkillName('');
+      setSkillLevel(0);
+      setSubmitDisabled(false);
+     alert("Success");
+    });
+  };
+
   return (
     <React.Fragment>
       <Title>Skills</Title>
@@ -60,16 +101,33 @@ export default function SkillsTable() {
           spacing={{xs: 2, md:2, lg: 2}}
         >
         <Grid xs={5} md={5} lg={5}>
-          <Input placeholder='Add skill name...' fullWidth/>
+          <Input
+            placeholder='Add skill name...'
+            fullWidth
+            value={skillName}
+            onChange={(event)=> {setSkillName(event.target.value)}}
+          />
         </Grid>
         <Grid xs={5} md={5} lg={5}> 
-          <Input placeholder='Add skill comfort level...' fullWidth/>
+          <Input
+            placeholder='Add skill comfort level...'
+            fullWidth
+            type='number'
+            value={skillLevel}
+            onChange={(event)=> {setSkillLevel(Number(event.target.value))}}
+          />
         </Grid>
         <Grid>
         </Grid>
       </Grid>
         <Grid container xs={2} md={2} lg={2} justifyContent="flex-end">
-          <Button variant="contained">
+          <Button
+            variant="contained"
+            onClick={() => {
+              handleSubmit();
+            }}
+            disabled={submitDisabled}
+          >
             Add Skill
           </Button>
         </Grid>
@@ -92,4 +150,6 @@ export default function SkillsTable() {
       </Table>
     </React.Fragment>
   );
-}
+})
+
+export default SkillsTable;
